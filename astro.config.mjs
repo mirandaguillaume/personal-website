a39b8f831772b0b43fe60ba309a11fa2aac3fd31
Guillaume MIRANDA <guillaume.miranda@consulting-for.edenred.com>
2026-08-18 17:10:02 +0200

    Ajout du routing i18n, du dictionnaire de traduction et du layout de base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    locales: ['fr', 'en'],
+    defaultLocale: 'fr',
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+});
---
+++ src/i18n/utils.ts
@@ -0,0 +1,14 @@
+import { ui, defaultLang, showDefaultLang } from './ui';
+import type { Lang } from './ui';
+
+export function useTranslations(lang: Lang) {
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    return ui[lang][key] ?? ui[defaultLang][key];
+  };
+}
+
+export function useTranslatedPath(lang: Lang) {
+  return function translatePath(path: string) {
+    return !showDefaultLang && lang === defaultLang ? path : `/${lang}${path}`;
+  };
+}
